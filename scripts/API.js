5bbe84e714306c54c756d0009080c4fadcb82907
Ben Jones <Ben@polyducks.co.uk>
2020-07-19 22:26:23 +0100

    move nav to side, rearrange

--- scripts/API.js
+++ scripts/API.js
@@ -70,6 +70,46 @@ API.user = {
 
 		if ( navigation ){
 
+			//MAP
+			(function(){
+
+
+				//<div id="wn-map">
+				//	<ul>
+				//		<li class="wn-populated">
+				//			<a class="wn-link" href="">
+				//				<span class="wn-title"></span>
+				//				<span class="wn-author"></span>
+				//				<span class="wn-desc"></span>
+				//			</a>
+				//		</li>
+				//		<li class="wn-unpopulated"></li>
+				//		...
+				//	</ul>
+				//</div>
+
+				var map = document.createElement("DIV");
+				map.id = "wn-map";
+				var ul = document.createElement("UL");
+				for ( var x = 0; x < API.settings.map_size; x++ ){
+					for ( var y = 0; y < API.settings.map_size; y++ ){
+						let li = document.createElement("LI");
+							var title = document.createElement("SPAN");
+								title.className = "wn-title";
+								li.appendChild(title);
+							var author = document.createElement("SPAN");
+								author.className = "wn-author";
+								li.appendChild(author);
+							var desc = document.createElement("SPAN");
+								desc.className = "wn-desc";
+								li.appendChild(desc);
+						ul.appendChild(li);
+					}
+				}
+				map.appendChild(ul);
+				navigation.appendChild(map);
+
+			})();
 
 			//Random page
 
@@ -95,53 +135,6 @@ API.user = {
 				navigation.appendChild(a);
 
 			})();
-
-
-
-			//MAP
-			(function(){
-
-				/*
-
-					<div id="wn-map">
-						<ul>
-							<li class="wn-populated">
-								<a class="wn-link" href="">
-									<span class="wn-title"></span>
-									<span class="wn-author"></span>
-									<span class="wn-desc"></span>
-								</a>
-							</li>
-							<li class="wn-unpopulated"></li>
-							...
-						</ul>
-					</div>
-
-				*/
-
-				var map = document.createElement("DIV");
-				map.id = "wn-map";
-				var ul = document.createElement("UL");
-				for ( var x = 0; x < API.settings.map_size; x++ ){
-					for ( var y = 0; y < API.settings.map_size; y++ ){
-						let li = document.createElement("LI");
-							var title = document.createElement("SPAN");
-								title.className = "wn-title";
-								li.appendChild(title);
-							var author = document.createElement("SPAN");
-								author.className = "wn-author";
-								li.appendChild(author);
-							var desc = document.createElement("SPAN");
-								desc.className = "wn-desc";
-								li.appendChild(desc);
-						ul.appendChild(li);
-					}
-				}
-				map.appendChild(ul);
-				navigation.appendChild(map);
-
-			})();
-
 
 		}
 	});
